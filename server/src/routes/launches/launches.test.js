8778bff66732fd3bf36e8db455b045386640c031
Mohamed <mohamed@accountable.eu>
2025-03-30 06:28:09 +0200

    Minor fix

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -3,6 +3,7 @@ const request = require("supertest");
 const app = require("../../app");
 
 const { mongoConnect, mongoDisconnect } = require("../../../utils/mongo");
+const { loadPlanetsData } = require("../../models/planets.model");
 
 jest.mock("passport", () => ({
   authenticate: () => (req, res, next) => next(),
